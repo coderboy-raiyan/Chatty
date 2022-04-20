184e1ad34e044ad7b72716c37b2fa786148e0b6e
Raiyan <raiyan5764@gmail.com>
2022-04-20 16:59:32 +0600

    eslint added in clinet

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/react-in-jsx-scope */
+// eslint-disable-next-line react/jsx-props-no-spreading
+import type { AppProps } from "next/app";
+import "styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
