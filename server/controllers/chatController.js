8e9399d6c3acca0a2143745fc4935fe59677c3b5
Raiyan <raiyan5764@gmail.com>
2022-04-25 13:52:35 +0600

    [ADD] access chat done

---
+++ server/controllers/chatController.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-underscore-dangle */
+
+const Chat = require('../models/Chat');
+const User = require('../models/User');
+const ErrorHandler = require('../lib/errorHandler');
+const { AsyncErrorHandler } = require('../middleware/createAsyncError');
+
+// create one to one chat
+module.exports.accessChat = AsyncErrorHandler(async (req, res, next) => {
+    const { userId } = req.body;
+
+    if (!userId) {
+        next(new ErrorHandler('UserId params not went with request'));
+    }
+
+    let isChat = await Chat.find({
+        isGroupChat: false,
+        $and: [
+            { users: { $elemMatch: { $eq: req.user._id } } },
+            { users: { $elemMatch: { $eq: userId } } },
+        ],
+    })
+        .populate('users', '-password')
+        .populate('latestMessages');
+
+    isChat = await User.populate(isChat, {
+        path: 'latestMessages.sender',
+        select: 'email pic name',
+    });
+
+    if (isChat.length < 0) {
+        res.send(isChat[0]);
+    } else {
+        const chatData = {
+            chatName: 'sender',
+            isGroupChat: false,
+            users: [req.user._id, userId],
+        };
+
+        try {
+            const createdChat = await Chat.create(chatData);
+            const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+                'users',
+                '-password',
+            );
+            console.log(createdChat);
+            res.status(200).send(fullChat);
+        } catch (error) {
+            next(new ErrorHandler(error));
+        }
+    }
+});
+
+// fetch chats
+module.exports.fetchChats = AsyncErrorHandler(async (req, res, next) => {});
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
+const verifyAuth = require('../middleware/authMiddleware');
+const { accessChat } = require('../controllers/chatController');
+
+router.route('/').post(verifyAuth, accessChat);
+// router.route("/group").post(createGroupChat)
+// router.router("/renameGroup").put(renameGroup)
+// router.route('/groupAdd').post(addToGroup)
+// router.route("/groupRemove").put(removeFromGroup)
 
 module.exports = router;
