17b8a292cb44c5ec8233a1ae7399e4ff91fa5351
Raiyan <raiyan5764@gmail.com>
2022-06-19 11:56:18 +0600

    server replaced

--- server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const verifyAuth = require('../middleware/authMiddleware');
-const { sendMessage, allMessages } = require('../controllers/messageController');
-
-router.route('/').post(verifyAuth, sendMessage);
-router.route('/:chatId').get(verifyAuth, allMessages);
-
-module.exports = router;
